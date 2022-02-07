c88cdea16a7101783d8d93c1725ec7ed145ddc81
formidablae <81068781+formidablae@users.noreply.github.com>
2022-02-07 02:17:13 +0100

    Solved problem 0014 Longest Collatz sequence

---
+++ 0014_Longest_Collatz_sequence.py
@@ -0,0 +1,21 @@
+collatz_sequences = {}
+collatz_sequences[1] = 1
+
+def len_of_collatz_sequence(n):
+    count = 1
+    if n in collatz_sequences.keys():
+        return collatz_sequences[n]
+    if n % 2 == 0:
+        count = 1 + len_of_collatz_sequence(n // 2)
+    else:
+        count = 1 + len_of_collatz_sequence(3 * n + 1)
+    collatz_sequences[n] = count
+    return count
+
+max_chain_length = 0
+for i in range(1, 1000000):
+    max_chain_length = max(max_chain_length, len_of_collatz_sequence(i))
+for l in collatz_sequences.keys():
+    if collatz_sequences[l] == max_chain_length:
+        print(l)
+        break
