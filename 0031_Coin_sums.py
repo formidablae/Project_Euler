a83b5757cb3445916a4ffe5b6ab3b448e41e72e0
formidablae <81068781+formidablae@users.noreply.github.com>
2022-03-12 16:01:03 +0100

    Solved 0031 Coin sums

---
+++ 0031_Coin_sums.py
@@ -0,0 +1,18 @@
+# In the United Kingdom the currency is made up of pound(£) and pence(p).
+# There are eight coins in general circulation:
+
+# 1p, 2p, 5p, 10p, 20p, 50p, £1 (100p), and £2 (200p).
+# It is possible to make £2 in the following way:
+
+# 1×£1 + 1×50p + 2×20p + 1×5p + 1×2p + 3×1p
+# How many different ways can £2 be made using any number of coins?
+
+def coin_sums(amount):
+    coins = [1, 2, 5, 10, 20, 50, 100, 200]
+    ways = [1] + [0] * amount
+    for coin in coins:
+        for i in range(coin, amount + 1):
+            ways[i] += ways[i - coin]
+    return ways[amount]
+
+print(coin_sums(200))
