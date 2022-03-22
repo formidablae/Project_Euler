d893f451b7d549ab7dc69ab206d00cab254ba70b
formidablae <81068781+formidablae@users.noreply.github.com>
2022-03-22 22:42:12 +0100

    Solved 0048 Self powers

---
+++ 0048_Self_powers/0048_Self_powers.py
@@ -0,0 +1,9 @@
+# The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+
+# Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+
+def main():
+    print(str(sum([i**i for i in range(1, 1001)]))[-10:])
+    
+if __name__ == '__main__':
+    main()
